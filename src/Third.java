a804043eae7ff1075b93755290bad6ee781b2510
PoliAlena <polivchak20042709@gmail.com>
2024-02-26 15:09:38 +0300

    Third!

--- src/Third.java
+++ src/Third.java
@@ -3,15 +3,25 @@ import java.util.Scanner;
 public class Third {
     public static void main(String [] args) {
         Scanner in = new Scanner(System.in);
-        System.out.print("Is number greater than 50: ");
-        int n = in.nextInt();
-        int a = 100;
-
-        if(n == 1){
-
+        int a = 50;
+        boolean win = false;
+        while (!win) {
+            System.out.print("Is number greater than "+a+"?: ");
+            int n = in.nextInt();
+            if (n == 0) {
+                System.out.print("Is number = "+a+"?: ");
+                n = in.nextInt();
+                if(n==0){
+                    a = a/2;
+                }
+                if(n==1){
+                    win = true;
+                }
+            }
+            if (n == 1) {
+                a = a*3/2;
+            }
         }
-        if(n == 0){
-
-        }
+        System.out.print("Win!Your number is"+ a);
     }
 }
